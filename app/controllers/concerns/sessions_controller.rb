6255c044dadbb1da2919ba6d292ac8b8ce218d2b
Ynda Jas <yndajas@gmail.com>
2021-02-25 13:45:34 +0000

    commit tree 219fe583555c30dff71558871a329f61447406ba

---
+++ app/controllers/concerns/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        user = User.find_by(name: params[:user][:name])
+        if user.try(:authenticate, params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to welcome_path
+        else
+            redirect_to login_path 
+        end
+    end
+end
+  
